314654aca61485d9d1f131b61b0e12a02740c86e
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-08-01 19:43:23 +0200

    Add complete Game loop

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -40,68 +39,91 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    switch (humanChoice) {
-        case "rock":
-            switch (computerChoice) {
-                case "rock":
-                    return "Draw! Rock against Rock!";
-                    break;
-                case "paper":
-                    return "You loose! Paper beats Rock!";
-                    computerScore++;
-                    break;
-                case "scissors":
-                    return "You win! Rock beats Scissors!";
-                    humanScore++;
-                    break;
-                default:
-                    return "default";
-        
-            }
-            break;
-        case "paper":
-            switch (computerChoice) {
-                case "rock":
-                    return "You win! Paper beats Rock!";
-                    humanScore++;
-                    break;
-                case "paper":
-                    return "Draw! Paper against Paper!";
-                    break;
-                case "scissors":
-                    return "You loose! Scissors beat Paper!";
-                    computerScore++;
-                    break;
-                default:
-                    return "default";
-        
-            }
-            break;
-        case "scissors":
-            switch (computerChoice) {
-                case "rock":
-                    return "You loose! Rock beats Scissors!";
-                    computerScore++;
-                    break;
-                case "paper":
-                    return "You win! Paper beats Scissors!";
-                    humanScore++;
-                    break;
-                case "scissors":
-                    return "Draw! Scissors agains Scissors!";
-                    break;
-                default:
-                    return "default";
-        
-            }
-            break;
-        default:
-            return "default";
 
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        switch (humanChoice) {
+            case "rock":
+                switch (computerChoice) {
+                    case "rock":
+                        return "Draw! Rock against Rock!";
+                        break;
+                    case "paper":
+                        computerScore++;
+                        return "You loose! Paper beats Rock!";
+                        break;
+                    case "scissors":
+                        humanScore++;
+                        return "You win! Rock beats Scissors!";
+                        break;
+                    default:
+                        return "default";
+            
+                }
+                break;
+            case "paper":
+                switch (computerChoice) {
+                    case "rock":
+                        humanScore++;
+                        return "You win! Paper beats Rock!";
+                        break;
+                    case "paper":
+                        return "Draw! Paper against Paper!";
+                        break;
+                    case "scissors":
+                        computerScore++;
+                        return "You loose! Scissors beat Paper!";
+                        break;
+                    default:
+                        return "default";
+            
+                }
+                break;
+            case "scissors":
+                switch (computerChoice) {
+                    case "rock":
+                        computerScore++;
+                        return "You loose! Rock beats Scissors!";     
+                        break;
+                    case "paper":
+                        humanScore++;
+                        return "You win! Paper beats Scissors!";
+
+                        break;
+                    case "scissors":
+                        return "Draw! Scissors agains Scissors!";
+                        break;
+                    default:
+                        return "default";
+            
+                }
+                break;
+            default:
+                return "default";
+    
+        }
+    }
+
+
+    for (let i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        console.log(playRound(humanChoice, computerChoice));
+    }
+    console.log("Your Score is: " + humanScore);
+    console.log("The Computer's score is: " + computerScore);
+    if(humanScore > computerScore){
+        console.log("You Win!!!");
+    } else if(computerScore > humanScore){
+        console.log("You Loose!!!")
+    } else {
+        console.log("Draw!!!")
     }
 }
-let hC = getHumanChoice();
-let cC = getComputerChoice();
 
-console.log(playRound(hC, cC))
+playGame();
